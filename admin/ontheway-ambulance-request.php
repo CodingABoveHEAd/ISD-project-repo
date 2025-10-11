d44a3b6fd38e4775460144f2c4c882de32b36f83
niloy <chowdhuryniloy819@gmail.com>
2025-10-11 16:11:58 +0600

    fixed the title to show the page name correctly

--- admin/ontheway-ambulance-request.php
+++ admin/ontheway-ambulance-request.php
@@ -11,7 +11,7 @@ if (strlen($_SESSION['eahpaid']==0)) {
 
 <!DOCTYPE html>
 <head>
-<title>Code Camp BD || On The Way Ambulance Request</title>
+<title>KUET Ambulance || On The Way Ambulance Request</title>
 
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- bootstrap-css -->
